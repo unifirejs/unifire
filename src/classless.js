9246fb2649564355086093ffb26f55a72f4c5f83
Joseph Podwys <joseph@podwys.com>
2019-11-17 18:43:57 -0700

    Add the ability to watch computed properties

--- src/classless.js
+++ src/classless.js
@@ -1,5 +1,6 @@
 const compute = (state, prop) => {
-  return isFunc(state[prop])
+  const val = state[prop];
+  return isFunc(val)
     ? val(state)
     : val;
 }
@@ -13,18 +14,17 @@ const isFunc = (val) => typeof val === 'function';
 
 const deref = (obj) => Object.assign({}, obj);
 
-const handler = {
-  get: (state, prop) => compute(state, prop),
-  set (state, prop, next) {
-    if (isFunc(state[prop])) return;
-    state[prop] = next;
-    return true;
-  }
-};
+const diff = (a, b) => Object.keys(a).filter(key => a[key] !== b[key]);
 
 export default function Unifire ({ state = {}, watch = {}, actions = {} }) {
   const subs = [];
-  this.state = new Proxy({}, handler);
+  this.state = new Proxy({}, {
+    get: (state, prop) => compute(state, prop),
+    set (state, prop, next) {
+      if (!isFunc(state[prop])) state[prop] = next;
+      return true;
+    }
+  });
   Object.assign(this.state, state);
 
   this.subscribe = (cb) => {
@@ -36,25 +36,26 @@ export default function Unifire ({ state = {}, watch = {}, actions = {} }) {
     const action = actions[name];
     if (!action) return;
     const context = {
-      set: this.set,
-      state: this.state,
-      fire: this.fire
+      set,
+      fire: this.fire,
+      state: this.state
     };
     action(context, payload);
   }
 
-  this.set = (delta, cb) => {
+  const set = (delta, cb) => {
     // Freeze state before mutations
     const before = deref(this.state);
     // Merge delta
     Object.assign(this.state, delta);
     // Freeze state after delta and computers
-    let after = deref(this.state);
+    const after = deref(this.state);
     // Given `before` and `after`, find changed keys and run watchers on them
-    const changedKeys = Object.keys(before).filter(key => before[key] !== after[key]);
+    // Watchers can mutate the `after` object
+    let changedKeys = diff(before, after);
     changedKeys.forEach(key => runWatch(watch, key, after[key], before[key], after));
-    // Recompute/freeze state after running watchers because watchers can also change state
-    after = deref(this.state);
+    // Get final list of changed keys
+    changedKeys = diff(before, after);
     // Call all subscribers and execute cb
     subs.forEach(sub => sub(changedKeys, before, after));
     if(cb) cb();
