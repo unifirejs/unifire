82194205d20b136fcddbe81a380ed0f99a6b7d30
Joseph Podwys <joseph@podwys.com>
2020-05-16 18:56:28 -0600

    ssh -> https

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jpodwys/unifire.git master:gh-pages
+git push -f https://github.com/jpodwys/unifire master:gh-pages
 
 cd -
